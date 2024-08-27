8dab5f3ee05e084ca253a72dea3c01ae68493807
Gustavo <gustavolovizotto@gmail.com>
2024-08-27 14:04:25 -0300

    Link of "Contact Us" page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Gustavo</h1>
+      <Link href="/contact">Fale conosco</Link>
     </div>
   );
 }
