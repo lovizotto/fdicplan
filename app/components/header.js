3c515c22e82e779bdc4526f0578e2b8fbd1e1b7a
Gustavo <gustavolovizotto@gmail.com>
2024-09-06 11:49:23 -0300

    Created the components folder and moved the header.js and footer.js

--- app/components/header.js
+++ app/components/header.js
@@ -1,22 +1,29 @@
+// app/components/header.js
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import MainNav from "./mainNav"; // Importa o MainNav
 
 export default function Header() {
   return (
     <header className="bg-zinc-600 p-4 flex justify-between items-center">
+      {/* Logo */}
       <div className="flex items-center ml-4">
-        <Link href="./">
+        <Link href="/">
           <Image
-            src="/images/image.png" 
+            src="/images/image.png"
             alt="FDICPLAN Logo"
-            width={130} 
+            width={130}
             height={100}
-            className="cursor-pointer" 
+            className="cursor-pointer"
           />
         </Link>
       </div>
 
+      {/* Navegação principal */}
+      <MainNav />
+
+      {/* Links adicionais */}
       <div className="flex space-x-6 mr-4">
         <Link href="#">
           <span className="text-white hover:text-orange-200">Add User</span>
