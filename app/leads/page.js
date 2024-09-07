a3c4ec84a7154fb4b646f667651ccd59731830e0
Gustavo <gustavolovizotto@gmail.com>
2024-09-06 23:23:42 -0300

    A draft od leads page

---
+++ app/leads/page.js
@@ -0,0 +1,14 @@
+export default function Leads() {
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold">Lista de Leads</h1>
+        <ul>
+          {/* Adicione um map para listar os leads */}
+          <li>Lead 1</li>
+          <li>Lead 2</li>
+          {/* Continue */}
+        </ul>
+      </div>
+    );
+  }
+  
