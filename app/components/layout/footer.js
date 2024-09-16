46682414144bc6816baa2ef55d9a6ba6504a3950
Gustavo <gustavolovizotto@gmail.com>
2024-09-15 21:40:49 -0300

    Changed the adress of component files

---
+++ app/components/layout/footer.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <footer className="bg-zinc-600 flex justify-between items-center text-white p-4 mt-auto">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Copyright */}
+        <div className="text-sm">
+          &copy; {new Date().getFullYear()} FDICPLAN. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ app/components/layout/mainNav.js
@@ -0,0 +1,24 @@
+'use client';
+import Link from 'next/link';
+
+export default function MainNav() {
+  return (
+    <nav className="flex space-x-6 ml-6 py-4 px-6">
+      <Link href="/prospects">
+        <span className="text-white hover:text-orange-200 transition duration-300">
+          Prospects
+        </span>
+      </Link>
+      <Link href="/leads">
+        <span className="text-white hover:text-orange-200 transition duration-300">
+          Leads
+        </span>
+      </Link>
+      <Link href="/contact">
+        <span className="text-white hover:text-orange-200 transition duration-300">
+          Contact
+        </span>
+      </Link>
+    </nav>
+  );
+}
