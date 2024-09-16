ff581e73438c63f3ca08ec203eb3cc039de5f8f2
Gustavo <gustavolovizotto@gmail.com>
2024-09-15 21:39:33 -0300

    Change the adress of component files

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
